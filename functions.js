07f6c1eda5c59cce993a998052018f0ffa058f4d
Jmyio <jimmyjimmy26282@gmail.com>
2024-05-13 18:08:53 +0800

    Initial commit

---
+++ functions.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var line1 = document.querySelector('.typewriter.line1');
+    var line2 = document.querySelector('.typewriter.line2');
+
+    setTimeout(function () {
+        line1.classList.add('hide-cursor');
+    }, 2100); // Hide cursor after 2 seconds
+
+    setTimeout(function () {
+        line2.style.visibility = 'visible';
+    }, 2100); // Make the second line visible after 2 seconds
+});
+
+import {skills, projects} from './listData.js';
+const skillsEntries = Object.entries(skills);
+const projectsEntries = Object.entries(projects);
+const skillsGrid = document.querySelector('.skills-grid');
+const projectsGrid = document.querySelector('.projects-grid');
+
+function setSkillsGrid() {
+    console.log('running');
+    skillsEntries.slice(0, 8).forEach(entry => {
+        const skillName = entry[0];
+        const skillIcon = entry[1];
+
+        const gridBlock = document.createElement('div');
+        gridBlock.className = 'grid-blocks';
+        const langContainer = document.createElement('div');
+        langContainer.className = 'lang-container';
+        const langIcon = document.createElement('i');
+        langIcon.className = skillIcon;
+        const langName = document.createElement('p');
+        langName.textContent = skillName;
+
+        langContainer.appendChild(langIcon);
+        langContainer.appendChild(langName);
+        gridBlock.appendChild(langContainer);
+        skillsGrid.appendChild(gridBlock);
+    });
+}
+
+function setProjectsGrid() {
+    console.log('running');
+    projectsEntries.forEach((entry, index) => {
+        const appName = entry[0];
+        const appLang = entry[1];
+
+        const projectBlock = document.createElement('div');
+        projectBlock.className = 'projects-blocks';
+        const projectImg = document.createElement('div');
+        projectImg.className = 'projects-img img' + (index + 1); // Fix: Use className instead of className
+        const layer = document.createElement('div');
+        layer.className = 'layer';
+        const name = document.createElement('p');
+        name.textContent = appName;
+        const progWifLine = document.createElement('p');
+        progWifLine.textContent = 'Programmed with:';
+        const langUsed = document.createElement('div');
+        langUsed.className = 'lang-used';
+        
+        appLang.forEach(lang => {
+            const langItem = document.createElement('i');
+            const langIcon = skills[lang];
+            langItem.className = langIcon;
+            langUsed.appendChild(langItem);
+        });
+
+        layer.appendChild(name);
+        layer.appendChild(progWifLine);
+        layer.appendChild(langUsed);
+        projectBlock.appendChild(projectImg);
+        projectBlock.appendChild(layer);
+        projectsGrid.appendChild(projectBlock);
+    });
+    const lastBlock = document.createElement('a');
+    lastBlock.className = 'projects-blocks last-block';
+    lastBlock.setAttribute('href', 'https://github.com/LeungYuLap19');
+    const lastBlockText = document.createElement('p');
+    lastBlock.textContent = 'More on Github';
+    const lastBlockArrow = document.createElement('i');
+    lastBlockArrow.className = 'fas fa-arrow-right';
+    lastBlock.appendChild(lastBlockText);
+    lastBlock.appendChild(lastBlockArrow);
+    projectsGrid.appendChild(lastBlock);
+}
+
+setSkillsGrid();
+setProjectsGrid();
---
+++ listData.js
@@ -0,0 +1,27 @@
+const skills = {
+    'Java': 'devicon-java-plain', 
+    'Javascript': 'devicon-javascript-plain', 
+    'Python': 'devicon-python-plain', 
+    'React': 'devicon-react-original', 
+    'Node.js': 'devicon-nodejs-plain', 
+    'Android Dev': 'devicon-androidstudio-plain', 
+    'Machine Learning': 'devicon-pytorch-original',
+    'SQL': 'devicon-mariadb-original', 
+    'Firebase': 'devicon-firebase-plain',
+    'HTML': 'devicon-html5-plain',
+    'CSS': 'devicon-css3-plain',
+    'Spring': 'devicon-spring-plain',
+    'MongoDB': 'devicon-mongodb-plain'
+};
+
+const projects = {
+    'Group Management and Task Assignment app': ['Java', 'Android Dev', 'Firebase'],
+    'Recruitment Management System': ['Java', 'SQL'],
+    'Weather App': ['HTML', 'CSS', 'Javascript'],
+    'E-commerce Bike Store': ['React', 'Spring', 'SQL'],
+    'E-commerce Nutrition Store': ['React', 'Firebase'],
+    'TaskFlow: To-Do-List App': ['React', 'Spring', 'SQL'],
+    'E-Platform': ['React', 'Node.js', 'SQL'],
+    'Connectify: Social App': ['React', 'Node.js', 'MongoDB']
+};
+export { skills, projects };
